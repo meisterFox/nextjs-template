1866e89a23827f84a5b27e52088a128cc6e4381e
meisterFox <101595590+meisterFox@users.noreply.github.com>
2026-01-26 02:26:03 +0300

    build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,1 @@
-import { getAuthOptions } from '@/lib/auth'
-import NextAuth from 'next-auth'
-
-const { handlers } = await NextAuth({
-  ...getAuthOptions(),
-})
-
-export const { GET, POST } = handlers
+export { GET, POST } from '@/lib/auth'
